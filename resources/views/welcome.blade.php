8710b6ddd66787023bcbdc098b196946c492ae8e
Qwernz <andhidwilesmana2803@gmail.com>
2026-04-28 23:35:47 +0800

    ubah

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="icon" type="image/jpg" href="{{ asset('images/Logo.jpg') }}">
+    <link rel="icon" type="images/jpg" href="{{ asset('images/Logo.jpg') }}">
 
     <style>
         body {
